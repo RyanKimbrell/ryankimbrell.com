c022abd2030c1fa3415885c42f59e775d5d7c9d7
Ryan Kimbrell <rkimbrell94@gmail.com>
2020-11-29 16:48:32 -0700

    added resume/cv button functionality for single page view js

--- website/static/website/resume.js
+++ website/static/website/resume.js
@@ -2,3 +2,37 @@
 *  Single page views that switch depending on the button press
 *  one for my resume, and one for my CV
 */
+
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    document.getElementById('resume-container').style.display = "none";
+    document.getElementById('cv-container').style.display = "none";
+
+    // Form view button functionality
+    document.getElementById('resume-button').onclick = () => showRecommendationForm('resume-button');
+    document.getElementById('cv-button').onclick = () => showRecommendationForm('cv-button');
+
+});
+
+function showRecommendationForm(buttonId) {
+    switch(buttonId) {
+
+        case 'resume-button':
+
+            // Display the resume and hide the cv
+            document.getElementById('resume-container').style.display = "block";
+            document.getElementById('cv-container').style.display = "none";
+            
+            break;
+
+        case 'cv-button':
+
+            // Display the artists input form and hide the others
+            document.getElementById('resume-container').style.display = "none";
+            document.getElementById('cv-container').style.display = "block";
+
+            break;
+
+    }
+}
