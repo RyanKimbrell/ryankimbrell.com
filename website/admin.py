c5650ab5c8b6034ed2dcef86c62855454df491e2
Ryan Kimbrell <rkimbrell94@gmail.com>
2020-11-30 10:21:48 -0700

    began building mailing list funcitonality

--- website/admin.py
+++ website/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 
-from .models import SamplePack
+from .models import SamplePack, MailingListFollower
 
 class SamplePackAdmin(admin.ModelAdmin):
     list_display = ("title", "price", "description", "image", "samplepackfile")
 
-
+class MailingListFollowerAdmin(admin.ModelAdmin):
+    list_display = ("firstname", "lastname", "email")
 
 admin.site.register(SamplePack, SamplePackAdmin)
+admin.site.register(MailingListFollower, MailingListFollowerAdmin)
--- website/models.py
+++ website/models.py
@@ -1,12 +1,16 @@
 from django.db import models
 
-class SamplePack (models.Model):
+class SamplePack(models.Model):
     title = models.CharField(max_length=100)
     price = models.IntegerField()
     description = models.TextField()
     image = models.ImageField()
     samplepackfile = models.FileField()
 
+class MailingListFollower(models.Model):
+    firstname = models.CharField(max_length=64)
+    lastname = models.CharField(max_length=64)
+    email = models.EmailField()
 
 
 
