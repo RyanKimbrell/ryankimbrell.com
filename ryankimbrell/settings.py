e2880f8c08613a5ece13fc3354ccfe94e656de0d
Ryan Kimbrell <rkimbrell94@gmail.com>
2020-12-02 15:31:10 -0700

    added postgres DB settings for production mode

--- ryankimbrell/settings.py
+++ ryankimbrell/settings.py
@@ -74,12 +74,25 @@ WSGI_APPLICATION = 'ryankimbrell.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'ryankimbrell',
+            'USER': 'ryan',
+            'PASSWORD': '!8MuadDib',
+            'HOST': 'localhost',
+            'PORT': '',
+        }
+    }
 
 
 # Password validation
