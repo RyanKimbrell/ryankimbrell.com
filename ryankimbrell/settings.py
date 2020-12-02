52b3b148d92b3d00e9098c353b8ff068ebc6ba3f
Ryan Kimbrell <rkimbrell94@gmail.com>
2020-12-02 16:24:30 -0700

    troubleshooting nginx on domain

--- ryankimbrell/settings.py
+++ ryankimbrell/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*#6n$%vu8zl7qxj^oposh69g+$=qtgfi9_xtzyocx42qie%=c!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['128.199.11.3', 'ryankimbrell.com']
+ALLOWED_HOSTS = ['ryankimbrell.com', '128.199.11.3']
 
 
 # Application definition
