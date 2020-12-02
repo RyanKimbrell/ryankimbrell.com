e7bafedcd69114a285e1d89278101b4e2563eed3
Ryan Kimbrell <rkimbrell94@gmail.com>
2020-12-02 15:32:11 -0700

    Debug = False

--- ryankimbrell/settings.py
+++ ryankimbrell/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*#6n$%vu8zl7qxj^oposh69g+$=qtgfi9_xtzyocx42qie%=c!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['128.199.11.3', 'ryankimbrell.com']
 
