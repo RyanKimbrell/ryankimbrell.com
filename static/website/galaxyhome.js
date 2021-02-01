25d695685b83db7a71562a3ef4ac6c85d33f1a11
Ryan Kimbrell <rkimbrell94@gmail.com>
2021-02-01 13:15:27 -0700

    mispelled styles.css in galaxyhome.js import

--- static/website/galaxyhome.js
+++ static/website/galaxyhome.js
@@ -1,4 +1,4 @@
-import './style.css'
+import './styles.css'
 import * as THREE from './node_modules/three/build/three.module.js';
 import { OrbitControls } from './node_modules/three/examples/jsm/controls/OrbitControls.js'
 import * as dat from './node_modules/dat.gui'
