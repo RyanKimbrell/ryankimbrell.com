b6e4a28140c14aed4269df6c5107138f8afa1872
Ryan Kimbrell <rkimbrell94@gmail.com>
2021-02-19 19:07:27 -0700

    fixing filepaths

--- website/static/website/mymusic.js
+++ website/static/website/mymusic.js
@@ -1,9 +1,9 @@
 import './styles.css'
 import * as THREE from './node_modules/three/build/three.module.js'
 import { OrbitControls } from './node_modules/three/examples/jsm/controls/OrbitControls.js'
-import * as dat from './node_modules/dat.gui'
+import * as dat from './node_modules/dat.gui/build/dat.gui.module.js'
 import { GLTFLoader } from './node_modules/three/examples/jsm/loaders/GLTFLoader.js'
-import { gsap } from './node_moduldes/gsap'
+import { gsap } from './node_moduldes/gsap/gsap-core.js'
 
 
 //====================================================================
