ab6a2cf855bb3bd6062f88aae4ad39a9f4593bae
Ryan Kimbrell <rkimbrell94@gmail.com>
2021-02-19 19:36:48 -0700

    type in import statement

--- static/website/mymusic.js
+++ static/website/mymusic.js
@@ -2,7 +2,7 @@ import * as THREE from './node_modules/three/build/three.module.js'
 import { OrbitControls } from './node_modules/three/examples/jsm/controls/OrbitControls.js'
 import * as dat from './node_modules/dat.gui/build/dat.gui.module.js'
 import { GLTFLoader } from './node_modules/three/examples/jsm/loaders/GLTFLoader.js'
-import gsap from './node_moduldes/gsap/gsap-core.js'
+import gsap from './node_modules/gsap/gsap-core.js'
 
 
 //====================================================================
